2e78d5dfadf1452379214d8b3f1641655d040966
RinnarBetta <rinnar007@gmail.com>
2024-09-27 19:22:56 +0300

    Updated

--- src/app/Push.java
+++ src/app/Push.java
@@ -1,7 +1,18 @@
 package app;
 
-class HelloWorld {
+public class Main {
+    private static final double CONV_K=1.60934;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Converter App.");
+        double miles=7;
+        double km = convmilesTokm(miles);
+        System.out.println("Resul is " + km + " kilomiters");
     }
+
+    private static double convmilesTokm(double miles){
+        return miles*CONV_K;
+    }
+
+
+
 }
